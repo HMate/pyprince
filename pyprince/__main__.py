b9ce1820c107b675b65724d6118baebf4ab36cbe
HMate <mhidvegi@gmail.com>
2021-09-24 15:19:38 +0200

    remove debug print

--- pyprince/__main__.py
+++ pyprince/__main__.py
@@ -5,14 +5,13 @@ from pyprince import parser, generators, serializer
 
 
 def main(entrypoint: pathlib.Path, draw_modules: bool = typer.Option(False, "--dm")):
-    typer.echo(f"Start is: {entrypoint.name}")
     mod = parser.parse_project(entrypoint)
     if draw_modules:
         desc = generators.describe_module_dependencies(mod)
         dj = serializer.to_json(desc)
-        print(dj.decode('utf-8'))
+        typer.echo(dj.decode("utf-8"))
     else:
-        print(generators.generate_code(mod))
+        typer.echo(generators.generate_code(mod))
 
 
 if __name__ == "__main__":
