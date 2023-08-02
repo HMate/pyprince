5e8e9d9cb5f963a1c313544f8bf3500e8f7c0451
Máté Hidvégi <mhidvegi@gmail.com>
2023-08-02 23:10:47 +0200

    rotate test logs for every run

--- tests/testutils/PyPrinceTestCase.py
+++ tests/testutils/PyPrinceTestCase.py
@@ -4,14 +4,33 @@ from typing import List
 
 from pyprince.logger import logger, get_log_folder
 
+g_log_should_rotate = True
+
 
 class PyPrinceTestCase(unittest.TestCase):
     """Contains additional asserts for usage in pyprince tests"""
 
     def __init__(self, methodName="runTest"):
         super().__init__(methodName)
-        logger.add(get_log_folder() / "pyprince_test.log", level="TRACE", diagnose=True, retention=10, rotation="00:00")
-        logger.debug(f"**** Starting test {methodName} ****")
+
+        def should_rotate_log(_msg, _file):
+            """We want to create a new log file for every every invocation of init()
+            So set the flag to False after the first log message.
+            """
+            global g_log_should_rotate
+            if g_log_should_rotate:
+                g_log_should_rotate = False
+                return True
+            return False
+
+        logger.add(
+            get_log_folder() / "pyprince_test.log",
+            level="TRACE",
+            diagnose=True,
+            retention=20,
+            rotation=should_rotate_log,
+        )
+        logger.info(f"**** Starting test {methodName} ****")
 
     def assertListElementsAreUnique(self, container: List, msg=None):
         uniques = set(container)
