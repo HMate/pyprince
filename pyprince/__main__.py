cd7629e84b4e5b16c1760ae43fe5a446a086e29d
Máté Hidvégi <mhidvegi@gmail.com>
2023-09-03 19:35:39 +0200

    add todo for 0.0.5

--- pyprince/__main__.py
+++ pyprince/__main__.py
@@ -6,5 +6,12 @@ from pyprince import console_entry_main
 #   Current cause is orjson. Either remove orjson, or see how can we make it compatible with multiple versions.
 # - Add tests for the npm package for every python version.
 
+# TODO: 0.0.5:
+# - Convert entry to flask app
+# - Parse project divided by modules.
+# - api endpoint to parse selected module by name
+# - api endpoint to parse from entrypoint script and shallow parse other modules
+# - api endpoint to parse all recusively.
+
 if __name__ == "__main__":
     console_entry_main()
